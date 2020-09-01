71e410ba0980c9c20151c62ed29e31407cf5d1fe
erin.olde <erin.olde@gmail.com>
2020-09-01 13:09:25 -0600

    made standalone

--- scripts/genbank_submission.sh
+++ scripts/genbank_submission.sh
@@ -1,49 +1,45 @@
 workdir=$1
-samples=$2
+samples=($2)
 
-if [ ! -f "$workdir/covid_samples.txt" ] ; then echo "File with metadata for genbank header not found" ; exit ; fi
-if [ -z "$sample" ]
-then
-  samples=($(tail -n+2 $workdir/covid_samples.txt | awk '{print $1}' ))
-fi
-echo ${samples[@]}
+if [ -z "$workdir" ] ; then workdir=$(pwd) ; fi
+if [ ! -f "$workdir/covid_samples.txt" ] ; then echo "File with metadata for genbank header not found at $workdir/covid_samples.txt" ; exit ; fi
+mkdir -p $workdir/covid/submission_files
 
+if [ -z "$sample" ]; then samples=($(tail -n+2 $workdir/covid_samples.txt | awk '{print $1}' | sort | uniq )) ; fi
 header_columns=($(head -n 1 $workdir/covid_samples.txt ))
-echo ${#header_columns[@]}
-
-for (( i=0 ; i<${#header_columns[@]}; i++ ))
-do
-  echo $i
-  echo ${header_columns[$i]}
-done
+submission_id_columns=($(history -p ${header_columns[@]} | grep -in "submission" | head -n 1 | cut -f 1 -d ":" ))
+if [ -z "$submission_id_columns" ] ; then submission_id_columns=1 ; fi
 
 for sample in ${samples[@]}
 do
-  fasta_header=$(echo ">$sample ")
+  line=($(grep $sample $workdir/covid_samples.txt | head -n 1 ))
+  submission_id=${line[$(($submission_id_columns - 1))]}
+
+  fasta_header=$(echo ">$submission_id ")
   for (( i=0 ; i<${#header_columns[@]}; i++ ))
   do
-    echo $i
-    echo "addition check $((i + 1 ))"
-    echo ${header_columns[$i]}
-    grep $sample $workdir/covid_samples.txt | cut -f $((i + 1 ))
-    value=$(grep $sample $workdir/covid_samples.txt | cut -f $((i + 1 )) | head -n 1)
-    fasta_header="$fasta_header[${header_columns[$i]}=$value]"
-    echo $fasta_header
-    i=$((i + 1 ))
+    header_test=$(echo ${header_columns[$i]} | grep -i -e "accession" -e "EpiTrax" -e "confidential" -e "zip" -e "id" )
+    if [ -z "$header_test" ]
+    then
+      fasta_header="$fasta_header[${header_columns[$i]}=${line[$i]}]"
+    fi
   done
-  echo $fasta_header
+  organism_test=$(echo $fasta_header | grep -i "organism")
+  if [ -z "$organism_test" ] ; then fasta_header="$fasta_header[organism=Severe acute respiratory syndrome coronavirus 2]" ; fi
+  host_test=$(echo $fasta_header | grep -i "host")
+  if [ -z "$host_test" ] ; then fasta_header="$fasta_header[host=human]" ; fi
+  country_text=$(echo $fasta_header | grep -i "country")
+  if [ -z "$country_text" ] ; then fasta_header="$fasta_header[country=USA]" ; fi
+  year_test=$(echo $fasta_header | grep -i "year" )
+  if [ -z "$year_test" ] ; then year=$(date +"%Y") ; fi
+  isolate_test=$(echo $fasta_header | grep -i "isolate")
+  if [ -z "$organism_test" ]
+  then
+    host=$(echo $fasta_header | sed 's/.*host=//g' | sed 's/\].*//g' )
+    country=$(echo $fasta_header | sed 's/.*country=//g' | sed 's/\].*//g' )
+    if [ -z "$year" ] ; then year=$(echo $fasta_header | sed 's/.*year=//g' | sed 's/\].*//g' ) ; fi
+    fasta_header="$fasta_header[isolate=SARS-CoV-2/$host/$country/$submission_id/$year]"
+  fi
+  echo $fasta_header > $workdir/covid/submission_files/$submission_id.genbank.fa
+  grep -v ">" $workdir/covid/consensus/$sample*fa* | sed 's/^N*N//g' | fold -w 75 >> $workdir/covid/submission_files/$submission_id.genbank.fa
 done
-
-exit
-if [ -z $sample ]
-then
-
-  $workdir/covid_samples.txt
-
-
-fi
-
-
-
-echo ">!{submission_id} [organism=Severe acute respiratory syndrome coronavirus 2][isolate=SARS-CoV-2/Human/USA/!{submission_id}/!{params.year}][host=Human][country=USA][collection_date=!{collection_date}]" > covid/submission_files/!{submission_id}.genbank.fa  2>> $err_file
-grep -v ">" !{consensus} | sed 's/^N*N//g' | fold -w 75 >> covid/submission_files/!{submission_id}.genbank.fa  2>> $err_file
