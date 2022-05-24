836fc511dff40ca0e9fb14da389b9adc271e9bd6
Erin Young <erin.olde@gmail.com>
2022-05-24 17:44:09 -0600

    added seqyclean and fixed returns

--- bin/combine_results.py
+++ bin/combine_results.py
@@ -6,49 +6,91 @@ from os.path import exists
 pangolin_file='lineage_report.csv'
 nextclade_file='nextclade.csv'
 vadr_file='vadr.csv'
+freyja_file='aggregated-freyja.tsv'
+seqyclean_file='Combined_SummaryStatistics.tsv'
 summary_file='combined_summary.csv'
-seqyclean_file
 
 summary_df = pd.read_csv(summary_file, dtype = str)
-
-if exists(pangolin_file) :
-    pangolin_df = pd.read_csv(pangolin_file, dtype = str, usecols = ['taxon', 'status', 'scorpio_call', 'version', 'pangolin_version', 'pango_version', 'pangoLEARN_version'])
-    pangolin_df['simple_name'] = pangolin_df['taxon'].replace('Consensus_','', regex=True).replace('.consensus.*', '', regex=True)
-    pangolin_df=pangolin_df.add_prefix('pangolin_')
-
-    summary_df = pd.merge(summary_df, pangolin_df, left_on = 'sample', right_on = 'pangolin_simple_name', how = 'outer')
-
-    summary_df['sample_id'] = summary_df.loc[summary_df['sample_id'].ne('null'), 'pangolin_taxon']
-    summary_df['sample'] = summary_df.loc[summary_df['sample'].ne('null'), 'pangolin_simple_name']
-
-    summary_df.drop('pangolin_simple_name', axis=1, inplace=True)
-    summary_df.drop('pangolin_taxon', axis=1, inplace=True)
-
-if exists(nextclade_file) :
-    nextclade_df = pd.read_csv(nextclade_file, sep = ';' , dtype = str, usecols = ['seqName', 'clade', 'qc.overallStatus', 'qc.overallScore'])
-    nextclade_df['simple_name'] = nextclade_df['seqName'].replace('Consensus_','', regex=True).replace('.consensus.*', '', regex=True)
-    nextclade_df=nextclade_df.add_prefix('nextclade_')
-
-    summary_df = pd.merge(summary_df, nextclade_df, left_on = 'sample', right_on = 'nextclade_simple_name', how = 'outer')
-
-    summary_df['sample_id'] = summary_df.loc[summary_df['sample_id'].ne('null'), 'nextclade_seqName']
-    summary_df['sample'] = summary_df.loc[summary_df['sample'].ne('null'), 'nextclade_simple_name']
-
-    summary_df.drop('nextclade_simple_name', axis=1, inplace=True)
-    summary_df.drop('nextclade_seqName', axis=1, inplace=True)
+columns = list(summary_df.columns)
+columns.remove('sample_id')
+columns.remove('sample')
+columns.remove('fasta_line')
 
 if exists(vadr_file) :
-    vadr_df = pd.read_csv(vadr_file, dtype = str, usecols = ['name', 'p/f', 'model', 'alerts'])
-    vadr_df['simple_name'] = vadr_df['name'].replace('Consensus_','', regex=True).replace('.consensus.*', '', regex=True)
+    print("Getting results from vadr file " + vadr_file)
+    vadr_df = pd.read_csv(vadr_file, dtype = str, usecols = ['name', 'p/f', 'model', 'alerts'], index_col= False)
     vadr_df=vadr_df.add_prefix('vadr_')
+    vadr_columns = list(vadr_df.columns)
+    vadr_columns.remove('vadr_name')
+    vadr_columns.remove('vadr_p/f')
 
-    summary_df = pd.merge(summary_df, vadr_df, left_on = 'sample', right_on = 'vadr_simple_name', how = 'outer')
-
-    # This currently erases the columns that were there for some reason
-    #summary_df['sample_id'] = summary_df.loc[summary_df['sample_id'].ne('null'), 'vadr_name']
-    #summary_df['sample'] = summary_df.loc[summary_df['sample'].ne('null'), 'vadr_simple_name']
-
-    summary_df.drop('vadr_simple_name', axis=1, inplace=True)
+    summary_df = pd.merge(summary_df, vadr_df, left_on = 'fasta_line', right_on = 'vadr_name', how = 'outer')
+    summary_df['sample_id'].fillna(summary_df['vadr_name'], inplace=True)
+    summary_df['fasta_line'].fillna(summary_df['vadr_name'], inplace=True)
     summary_df.drop('vadr_name', axis=1, inplace=True)
+    columns = ['vadr_p/f'] + columns + vadr_columns
 
-summary_df.to_csv('cecret_results.csv')
+if exists(nextclade_file) :
+    print("Getting results from nextclade file " + nextclade_file)
+    nextclade_df = pd.read_csv(nextclade_file, sep = ';' , dtype = str, usecols = ['seqName', 'clade', 'qc.overallStatus', 'qc.overallScore'])
+    nextclade_df=nextclade_df.add_prefix('nextclade_')
+    nextclade_columns = list(nextclade_df.columns)
+    nextclade_columns.remove('nextclade_seqName')
+    nextclade_columns.remove('nextclade_clade')
+
+    summary_df = pd.merge(summary_df, nextclade_df, left_on = 'fasta_line', right_on = 'nextclade_seqName', how = 'outer')
+    summary_df['sample_id'].fillna(summary_df['nextclade_seqName'], inplace=True)
+    summary_df['fasta_line'].fillna(summary_df['nextclade_seqName'], inplace=True)
+    summary_df.drop('nextclade_seqName', axis=1, inplace=True)
+    columns = ['nextclade_clade'] + columns + nextclade_columns
+
+if exists(pangolin_file) :
+    print("Getting results from pangolin file " + pangolin_file)
+
+    pangolin_df = pd.read_csv(pangolin_file, dtype = str)
+    pangolin_df=pangolin_df.add_prefix('pangolin_')
+    pangolin_columns = list(pangolin_df.columns)
+    pangolin_columns.remove('pangolin_taxon')
+    pangolin_columns.remove('pangolin_lineage')
+
+    summary_df = pd.merge(summary_df, pangolin_df, left_on = 'fasta_line', right_on = 'pangolin_taxon', how = 'outer')
+    summary_df['sample_id'].fillna(summary_df['pangolin_taxon'], inplace=True)
+    summary_df['fasta_line'].fillna(summary_df['pangolin_taxon'], inplace=True)
+    summary_df.drop('pangolin_taxon', axis=1, inplace=True)
+    columns = ['pangolin_lineage'] + columns + pangolin_columns
+
+if exists(freyja_file) :
+    print("Getting results from freyja file " + freyja_file)
+    freyja_df = pd.read_table(freyja_file, dtype = str, sep="\t")
+    freyja_df = freyja_df.add_prefix('freyja_')
+    freyja_df['freyja_Unnamed: 0'] = freyja_df['freyja_Unnamed: 0'].str.replace("_variants.tsv", "")
+    freyja_columns = list(freyja_df.columns)
+    freyja_columns.remove('freyja_Unnamed: 0')
+
+    summary_df = pd.merge(summary_df, freyja_df, left_on = 'sample_id', right_on = 'freyja_Unnamed: 0', how = 'outer')
+    summary_df['sample_id'].fillna(summary_df['freyja_Unnamed: 0'], inplace=True)
+    summary_df['fasta_line'].fillna(summary_df['freyja_Unnamed: 0'], inplace=True)
+    summary_df.drop('freyja_Unnamed: 0', axis=1, inplace=True)
+    columns = columns + freyja_columns
+
+if exists(seqyclean_file) :
+    print("Getting results from seqyclean file " + seqyclean_file)
+    seqyclean_df = pd.read_table(seqyclean_file, dtype = str, sep="\t", usecols = ['OutputPrefix', 'PairsKept', 'Perc_Kept'])
+    seqyclean_df = seqyclean_df.add_prefix('seqyclean_')
+    seqyclean_df['seqyclean_OutputPrefix'] = seqyclean_df['seqyclean_OutputPrefix'].str.replace("seqyclean/", "")
+    seqyclean_df['seqyclean_OutputPrefix'] = seqyclean_df['seqyclean_OutputPrefix'].str.replace("_clean", "")
+    seqyclean_columns = list(seqyclean_df.columns)
+    seqyclean_columns.remove('seqyclean_OutputPrefix')
+
+    summary_df = pd.merge(summary_df, seqyclean_df, left_on = 'sample_id', right_on = 'seqyclean_OutputPrefix', how = 'outer')
+    summary_df['sample_id'].fillna(summary_df['seqyclean_OutputPrefix'], inplace=True)
+    summary_df['fasta_line'].fillna(summary_df['seqyclean_OutputPrefix'], inplace=True)
+    summary_df.drop('seqyclean_OutputPrefix', axis=1, inplace=True)
+    columns = columns + seqyclean_columns
+
+#summary_df.dropna(axis=1, how='all', inplace=True)
+summary_df.replace([" ", ",", "\t", "\n"], [" ", " ", " ", " "], regex=True, inplace=True)
+summary_df.sort_values(by=['sample_id'], ascending=True)
+summary_df.replace([" ", ",", "\t", "\n"], [" ", " ", " ", " "], regex=True, inplace=True)
+summary_df.drop_duplicates(keep='first', inplace=True)
+summary_df.to_csv('cecret_results.csv', columns = ['sample_id','sample'] + columns, index=False)
