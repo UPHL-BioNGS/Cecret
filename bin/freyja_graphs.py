91d4858a36d22ac68978534728d59b731ade78ae
Erin Young <erin.olde@gmail.com>
2023-05-31 17:04:04 -0600

    simplified freyja script

--- bin/freyja_graphs.py
+++ bin/freyja_graphs.py
@@ -5,54 +5,37 @@
 # adapted by Erin Young                                                      #
 ##############################################################################
 
-# Import required packages
 import pandas as pd
-import re
 
 freyja_file = 'freyja/aggregated-freyja.tsv'
 
-# Function to prepare lineage dictionary
-def prepLineageDict(agg_df):
-    # Split the 'lineages' and 'abundances' fields by spaces
-    agg_df.loc[:, 'lineages']   = agg_df['lineages'].apply(lambda x: re.sub(' +', ' ', x).split(' ')).copy()
-    agg_df.loc[:, 'abundances'] = agg_df['abundances'].apply(lambda x: re.sub(' +', ' ', x).split(' ')).copy()
-
-    return agg_df  # Return the updated DataFrame
-
 # Function to make pie charts for each sample
 def makePieCharts_simple(agg_df, lineages, outputFnBase):
-    # Prepare the lineage dictionary if lineages is given
-    if lineages:
-        queryType = 'linDict'
-        agg_df = prepLineageDict(agg_df)
-    else:
-        return
-
     # Loop over all samples in the DataFrame
     for k in range(0, agg_df.shape[0]):
         sample                  = agg_df['sample'].iloc[k]
         sample_df               = agg_df[agg_df['sample' ] == sample ].copy()
+        sample_df['lineages']   = sample_df['lineages'].str.split(" ")
+        sample_df['abundances'] = sample_df['abundances'].str.split(" ")
         sample_df               = sample_df.explode(['lineages', 'abundances'])
         sample_df['abundances'] = sample_df['abundances'].astype(float)
-
-        # reduce small numbers to 'other' lineage
-        try:
-            other_adundance     = sample_df[sample_df['abundances' ] < 0.05]['abundances'].sum()
-        except:
-            other_adundance     = 0.0
-
-        other_dict              = {'sample': [sample], 'lineages': ['other'], 'abundances': [other_adundance] }
-        other_df                = pd.DataFrame(other_dict)
         
-        sample_df               = pd.concat([sample_df, other_df ], ignore_index = True)
+        # reducing noise to lineages with 3% or more
+        sample_df               = sample_df[sample_df['abundances' ] >= 0.03 ]
+        other_adundance         = 1 - sample_df['abundances' ].sum()
+        if other_adundance > 0.001 :
+            other_dict          = {'sample': [sample], 'lineages': ['other'], 'abundances': [other_adundance] }
+            other_df            = pd.DataFrame(other_dict)
+            sample_df           = pd.concat([sample_df, other_df ], ignore_index = True)
+        
+        # adding in some spacing
         sample_df['explode']    = 0.025
-
-        sample_df               = sample_df[sample_df['abundances' ] >= 0.03]
-        
+       
         # creating the pie chart
         pie_df                  = sample_df.drop('sample', axis = 1)
         pie_df                  = pie_df.set_index('lineages')
-        plot = pie_df.plot.pie(y='abundances', figsize=(5, 5), autopct='%.2f', startangle=90, legend=False, explode=pie_df['explode'].tolist())
+        plot = pie_df.plot.pie(y='abundances', figsize=(5, 5), autopct='%.1f%%', startangle=90, legend=False, explode=pie_df['explode'].tolist())
+        plot.yaxis.set_visible(False)
         plot.get_figure().savefig('freyja/' + sample + '_freyja_lineages.png')
         plot.get_figure().savefig('freyja/' + sample + '_freyja_lineages_mqc.png')
 
