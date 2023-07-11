84064af56ac04cf3fb467b195eb7122d8414f0e7
Erin Young <erin.olde@gmail.com>
2023-07-11 11:33:34 -0600

    adjusted graph

--- bin/primerassessment.py
+++ bin/primerassessment.py
@@ -2,14 +2,21 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+
+os.mkdir('primerassessment')
 
 df = pd.read_csv("primer_assessment.csv")
 df.drop('sample', axis=1, inplace=True)
 
-png = plt.figure()
+boxplot = df.boxplot(fontsize=5, 
+                     rot=90, 
+                     figsize=(15,8), 
+                     grid=False)
+boxplot.plot()
 plt.title('Primer Assessment')
-boxplot = df.boxplot(fontsize=5, rot=90, figsize=(15,8), patch_artist=True)
 boxplot.set_ylabel('meandepth')
-boxplot.set_xlabel('primer name')
-png.savefig('primerassessment/primerassessment.png',     format = 'png')
-png.savefig('primerassessment/primerassessment_mqc.png', format = 'png')
+boxplot.set_xlabel('amplicon name')
+boxplot.figure.savefig('primerassessment/primerassessment.png',     bbox_inches='tight')
+boxplot.figure.savefig('primerassessment/primerassessment_mqc.png', bbox_inches='tight')
+
